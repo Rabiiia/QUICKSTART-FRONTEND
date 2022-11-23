9d653cb45f1ec1eea90f32bd4572605c71412abb
Rabiiia <74189957+Rabiiia@users.noreply.github.com>
2022-11-23 07:37:09 +0100

    Update settings.js

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:8080"
+export const DROPLET_FOLDER = "/XXX" // <-- replace with your folder that your create on droplet 
+export const BASE_URL = "http://localhost:8080" // <-- replace it with your droplet url so that you can use your database on your droplet instedet local database
 export const LOGIN_URL = BASE_URL + "/api/login"
 export const VERIFY_URL = BASE_URL + "/api/verify"
