bff7d800ad922dd463917bf1d23ee5d55577da0e
Rabiiia <74189957+Rabiiia@users.noreply.github.com>
2022-11-23 10:11:14 +0100

    Update LoggedIn.jsx

--- src/components/LoggedIn.jsx
+++ src/components/LoggedIn.jsx
@@ -16,6 +16,8 @@ export default function LoggedIn({user,setUser}) {
             {user.isLoggedIn && <span>Hi, {user["username"]}! {user["roles"].includes("admin") && //replaced
             <span className="badge bg-dark">Admin</span>}</span>} 
             <button onClick={logout}>Logout</button>
+            {/* But when you deploy then the button above - >
+            <Link to={DROPLET_FOLDER} onClick={logout}>Logout</Link>. You can make it into a button inside Link i guess but dont know how */}
         </div>
     )
 
