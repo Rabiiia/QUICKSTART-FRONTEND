71f3ab777f760cb34d66a9017971c15896c512cb
Rabiiia <74189957+Rabiiia@users.noreply.github.com>
2022-11-23 09:12:19 +0100

    Update settings.js

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,4 +1,4 @@
-export const DROPLET_FOLDER = "/XXX" // <-- replace with your folder that your create on droplet 
+export const DROPLET_FOLDER = "/XXX" // <-- replace with your folder that your create on droplet. This will play huge role whenever you use NavLink and Link components 
 export const BASE_URL = "http://localhost:8080" // <-- replace it with your droplet url so that you can use your database on your droplet instead of local database
 export const LOGIN_URL = BASE_URL + "/api/login"
 export const VERIFY_URL = BASE_URL + "/api/verify"
